d5748533162115382e5fba9583b024f8d83958ed
Serhii Khabuzov <habuzovs@gmail.com>
2024-02-02 10:17:27 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    target: 'experimental-serverless-trace',
+  };
+  
+  export default nextConfig;
