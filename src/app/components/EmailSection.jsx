bd2fd43f9fd731e69af652e4cf0b97650daec75e
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-02-03 17:06:44 +0100

    added footer

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -4,40 +4,12 @@ import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
+import { useForm, ValidationError } from '@formspree/react';
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+    const [state, handleSubmit] = useForm("mgegaklk");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
-  };
 
   return (
     <section
@@ -65,7 +37,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        {emailSubmitted ? (
+        {state.succeeded ? (
           <p className="text-green-500 text-sm mt-2">
             Email sent successfully!
           </p>
