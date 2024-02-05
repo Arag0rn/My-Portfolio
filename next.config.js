63cc0efb3d2cb779546033868c313f4d4ca09f83
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-02-05 20:31:16 +0100

    fixrd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const withNextIntl = createNextIntlPlugin();
-
-export default withNextIntl
+module.exports = nextConfig
