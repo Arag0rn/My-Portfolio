488fea8a74d065e0ca5d37c4d6ba2b17e4ebd752
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-02-05 19:54:30 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl({
   basePath: '',
   assetPrefix: '',
-});
+  outDir: './out',
+}); 
