24a9de215892709bcb2850c1e409eb8445d59da2
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-07-20 14:13:01 +0200

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -4,9 +4,8 @@ import Image from "next/image";
 import TabButton from "./TabButton";
 import { useTranslations } from 'next-intl';
 import CertModal from "./CertModal";
-import goit from "../../../public/certification/GOIT.jpeg";
-import zer from "../../../public/certification/zer.jpeg";
-
+const goit = '/certification/GOIT.jpeg';
+const zer = '/certification/zer.jpeg';
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
   const [open, setOpen] = useState(false);
