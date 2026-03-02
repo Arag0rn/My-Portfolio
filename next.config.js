5c4b54faad16bc932a24151c2b49b6c82186e7ab
Serhii Khabuzov <habuzovs@gmail.com>
2026-03-02 15:03:16 +0100

    delete midleware

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin(
-  './src/i18n/request.ts'
-);
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
